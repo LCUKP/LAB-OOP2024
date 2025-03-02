ad48b34f0637e7f1156b755d4b9c9a91feaec83e
LCUKP <puwanatjaijitjaam@gmail.com>
2025-03-03 01:33:32 +0700

    Lab11

---
+++ LAB/src/Lab11/CheckingAccount.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab11;
+
+public class CheckingAccount extends Account {
+    private double credit;
+
+    public CheckingAccount() {
+        super(0.0, "");
+        this.credit = 0.0;
+    }
+    
+    public CheckingAccount(double balance, String name, double credit) {
+        super(balance, name);
+        this.credit = credit;
+    }
+
+    public void setCredit(double credit) {
+        if (credit > 0){
+            this.credit = credit;
+        } else {
+            System.out.println("Input number must be a positive integer.");
+        }
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    @Override
+    public void withdraw(double a) throws WithdrawException {
+        if (a > 0 && (balance - a >= 0)) {
+            balance -= a;
+            System.out.println(a + " baht is withdrawn from " + name + " and your credit balance is " + credit +".");
+        } else if (a > 0 && ((balance + credit) - a >= 0)) {
+            balance -= a;
+            credit += balance;
+            balance = 0;
+            System.out.println(a + " baht is withdrawn from " + name + " and your credit balance is " + credit +".");
+        } else if (a > 0 && (balance - a) < 0) {
+            throw new WithdrawException("Account " + name + " has not enough money.");
+        } else {
+            System.out.println("Input number must be a positive integer.");
+        }
+    }
+    
+    public void withdraw(String a) throws WithdrawException {
+        this.withdraw(Double.parseDouble(a));
+    }
+
+    @Override
+    public String toString() {
+        return "The " + name + " account has " + balance + " baht and " + credit + " credits.";
+    }
+}
